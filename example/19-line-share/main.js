c16bd1864fdf7c5d4e4dfb17bf547dd335a560cd
MikeCheng1208 <a3804430@hotmail.com>
2017-09-27 16:58:20 +0800

    line share fix

--- example/19-line-share/main.js
+++ example/19-line-share/main.js
@@ -5,11 +5,11 @@ window.onload = function(){
     }else{
         document.getElementById("LinePc").style.display="none";
     }
-	
+    
     document.getElementById("LinePc").onclick = function(){
-        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
     document.getElementById("Linemobile").onclick = function(){
-        window.open('line://msg/text/'+ encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('line://msg/text/'+ encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
 }
